6bbb3bcdb2516e58caf8ed1f8cf4302575bdacd6
Guilherme <guilherme.breda@instruct.com.br>
2021-06-05 17:13:31 -0300

    Creates index view

---
+++ src/views/index/index.js
@@ -0,0 +1,8 @@
+import BaseButton from '../../shared/components/base-button/base-button.vue';
+
+export default {
+  name: 'home',
+  components: {
+    BaseButton,
+  },
+};
---
+++ src/views/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <img
+        class="container__logo"
+        src="@/assets/img/logo.svg"
+        alt="Rick and Morty logo"
+      />
+    </div>
+    <base-button>Search</base-button>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+<style src="./index.scss" lang="scss" scoped></style>
