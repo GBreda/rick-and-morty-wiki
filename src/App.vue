b79f18c1a4d9e1b62d2c7e96d5784ba9777f6f54
Guilherme <guilherme.breda@instruct.com.br>
2021-06-04 22:05:44 -0300

    Remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
 <template>
-  <div id="app">
-    <h1>Deploy Teste</h1>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div></div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script src="./app.js"></script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/rick-and-morty-wiki/' : '/',
+    process.env.NODE_ENV === 'production' ? process.env.PUBLIC_PATH : '/',
 };
