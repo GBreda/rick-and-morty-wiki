bd778bfc720897058b6b65165ad5b012b392dc2c
Guilherme <guilherme.breda@instruct.com.br>
2021-06-05 17:12:52 -0300

    Creates shared button component

---
+++ src/shared/components/base-button/base-button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button class="base-button">
+    <slot />
+  </button>
+</template>
+
+<script src="./base-button.js"></script>
+<style src="./base-button.scss" lang="scss" scoped></style>
