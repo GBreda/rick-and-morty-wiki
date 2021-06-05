23bba26b816fdc11750be23c1c80053382537d56
Guilherme <guilherme.breda@instruct.com.br>
2021-06-05 13:01:17 -0300

    Add suport do Apollo GraphQl

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueApollo from 'vue-apollo';
+import apolloProvider from '@/middleware/apollo';
 
 import '@/assets/scss/main.scss';
 
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
 
 new Vue({
+  apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
