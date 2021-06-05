09905febb1ba247c2a69e50f6a6ec84f2e2121a0
Guilherme <guilherme.breda@instruct.com.br>
2021-06-05 13:14:11 -0300

    Add suport to Vue router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
-import App from './App.vue';
 import VueApollo from 'vue-apollo';
 import apolloProvider from '@/middleware/apollo';
+import router from './router';
+import App from './App.vue';
 
 import '@/assets/scss/main.scss';
 
 Vue.config.productionTip = false;
+
 Vue.use(VueApollo);
 
 new Vue({
+  router,
   apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
